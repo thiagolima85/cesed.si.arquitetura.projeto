f80764abb5bdb8071202704a4b4efd0a8db0f2fb
Thiago Gonçalves <thiagolima85@gmail.com>
2015-11-19 22:26:28 -0300

    Classe de contexto + EntityFramework

---
+++ cesed.si.dotnet.projeto.infra.data/Contexto/Contexto.cs
@@ -0,0 +1,18 @@
+﻿
+
+using cesed.si.dotnet.projeto.dominio.Entidades;
+using System.Data.Entity;
+
+namespace cesed.si.dotnet.projeto.infra.data.Contexto
+{
+    public class Contexto : DbContext
+    {
+        public Contexto()
+            : base("ProjetoDotNet")
+        {
+
+        }
+
+        public DbSet<Usuario> Usuarios { get; set; }
+    }
+}
